a8bfb2de2e4e76db8730b6bdd97bc57274f0ca29
EmoPorEmilio <franceschini.emilio@gmail.com>
2024-09-26 12:54:01 -0300

    5

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,12 +4,12 @@ interface Props {
   children?: any
 }
 const Header = ({ first_word, second_word, children }: Props) => (
-  <nav class="flex items-center bg-bg-400 h-[70px] border-bottom border-solid border-b-4 border-accent-500 font-oswald uppercase subpixel-antialiased">
-    <span class="md:pl-3 pl-3 text-[34px] font-light text-primary-800 shadow-none">
+  <nav class="flex items-center bg-bg-400 h-[48px] border-bottom border-solid border-b-2 border-secondary-100 font-oswald uppercase subpixel-antialiased text-lg font-semibold">
+    <span class="md:pl-3 pl-3 text-secondary-100">
       {first_word}
     </span>
 
-    <div class="text-primary-500 font-black drop-shadow-logo-white">
+    <div class="text-primary-600">
       <span class="z-1 text-[35px]">{second_word}</span>
     </div>
     {children}
